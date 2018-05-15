7dde85b808498c6621ffe8e24505a5b3790c94ec
Reese Akamine <r.akamine@Reeses-MacBook-Pro.local>
2018-05-15 11:03:59 -1000

    setup, working on home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'; // for http
+import { RouterModule } from '@angular/router'; // for routing
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(
+      [
+        { path: '', component: HomeComponent },
+        { path: '**', redirectTo: '', pathMatch: 'full' }
+      ],
+      { enableTracing: true }
+    ),
   ],
   providers: [],
   bootstrap: [AppComponent]
