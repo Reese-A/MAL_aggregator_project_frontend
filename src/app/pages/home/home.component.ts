8d02799177bbf8876bc645dec1d3460cc21a5294
Reese Akamine <r.akamine@Reeses-MacBook-Pro.local>
2018-05-15 16:42:27 -1000

    css for home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,13 +7,16 @@ import { SearchService } from '../../services/search.service';
 })
 export class HomeComponent {
   title: string;
+  user: Object = {
+    name: '',
+  };
+  userSeries: Object[];
   malUser: Object = {
     name: ''
   };
 
   constructor(private searchService: SearchService) {
     this.title = 'MAL Ratings Aggregator' || '';
-
   }
 
   submit(e) {
@@ -23,6 +26,18 @@ export class HomeComponent {
       .toPromise()
       .then((data) => {
         console.log(data);
+        this.user.name = data.myanimelist.myinfo.user_name;
+        const sortedSeries = data.myanimelist.anime.sort(function compare(a, b) {
+          if (Number(a.my_score) > Number(b.my_score)) {
+            return -1;
+          }
+          if (Number(a.my_score) < Number(b.my_score)) {
+            return 1;
+          }
+          return 0;
+        });
+        this.userSeries = sortedSeries;
+        console.log(this.userSeries);
       })
       .catch((err) => {
         console.log(err);
