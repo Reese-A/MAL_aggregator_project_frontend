79134aa59ab1150940afe1efc318428973151777
Reese Akamine <r.akamine@Reeses-MacBook-Pro.local>
2018-05-17 16:14:44 -1000

    group component rankings WIP

--- src/app/pages/group/group.component.ts
+++ src/app/pages/group/group.component.ts
@@ -10,10 +10,17 @@ import { SearchService } from '../../services/search.service';
 export class GroupComponent {
   userNames: Object[];
   usersData: Object[];
+  usersSeries: Object[];
+  groupSeries: Object[];
+  groupRankings: Object[];
 
   constructor(private groupService: GroupService, private searchService: SearchService) {
     this.userNames = [];
     this.usersData = [];
+    this.usersSeries = [];
+    this.groupSeries = [];
+    this.groupRankings = [];
+
     this.groupService.getGroup()
 
       .toPromise()
@@ -23,22 +30,44 @@ export class GroupComponent {
         });
         return this.userNames;
       })
+
       .then((names) => {
         names.forEach((username) => {
           this.searchService.getData(username)
-          .toPromise()
-          .then((data) => {
-            return this.usersData.push(data);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
+
+            .toPromise()
+            .then((data) => {
+              return this.usersData.push(data);
+            })
+            .then((data) => {
+              return this.usersSeries = this.usersData.map(function (user) {
+                return user['myanimelist']['anime'];
+              });
+            })
+            .then((data) => {
+              console.log(this.usersSeries);
+              for (let i = 0; i < this.usersSeries.length; i++) {
+                for (let j = 0; j < this.usersSeries[i].length; j++) {
+                  const userShows = Object.values(this.usersSeries[i][j]);
+                  const show = {};
+                  show[userShows[1]] = userShows[13];
+                  this.groupRankings.push(show);
+                  console.log(this.groupRankings);
+                  if (!this.groupSeries.includes(this.usersSeries[i][j])) {
+                    this.groupSeries.push(this.usersSeries[i][j]);
+                  }
+                }
+              }
+              console.log('Group Series: ', this.groupSeries);
+            })
+            .catch((err) => {
+              console.log(err);
+            });
         });
-        return this.usersData;
       })
-      .then((data) => {
-        console.log(data);
-        console.log('USERS DATA: ', this.usersData);
+
+      .catch((err) => {
+        console.log(err);
       });
   }
 
